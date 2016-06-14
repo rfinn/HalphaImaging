29202668ad13d238b7c9097e0eea8377b3ba5b26
rfinn@siena.edu <rfinn@siena.edu>
2016-06-14 12:39:20 -0400

    bug in header?

--- uat_HDIfixheader.py
+++ uat_HDIfixheader.py
@@ -102,7 +102,7 @@ for f in files:
         header.append(card=('CRPIX2',(naxis2+1)/2.,'Y reference pixel'))
 
     if 'CD1_1' in header:
-        header['CD1_1']=(-1.*float(args.pixelscalex),'Pixel scale in X')
+        header['CD1_1']=(float(args.pixelscalex),'Pixel scale in X')
         header['CD2_2']=(float(args.pixelscaley),'Pixel scale in Y')
     else:
         header.append(card=('CD1_1',-1.*float(args.pixelscalex),'Pixel scale in X'))
