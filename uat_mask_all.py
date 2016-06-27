860569569e5909590bebaa408d197cf37209d2c4
rfinn@siena.edu <rfinn@siena.edu>
2016-06-27 19:52:45 -0400

    updates to uat codes

--- uat_mask_all.py
+++ uat_mask_all.py
@@ -3,39 +3,38 @@
 '''
 PURPOSE:
 
-The goal of the program is to call uat_mask.py for all files that
-match the input file string.
+The goal of the program is to call uat_mask.py for all
+R-band images with a prefix that matches the input string.  
 
 USAGE:
 
-from within ipython type:
+~/github/HalphaImaging/uat_mask_all.py --string 'A1367'
 
-   %run ~/github/HalphaImaging/uat_mask.py --image 'A1367-140231-R.fits'
+This will find all images that match A1367*-R.fits and
+run uat_mask.py on each image.
 
-you just need to run this on R-band images.
+We use the same mask for the R and Halpha images, so you
+just need to run this on R-band images.
 
 
 PROCEDURE:
 
+- construct match string from input prefix
+- get list of files that match string
+- call uat_mask.py for each image
+
 
 REQUIRED MODULES:
-   os
-   astropy
-   numpy
-   argsparse
-   matplotlib
-   scipy
 
+os
+argsparse
+glob
 
 '''
 
 import os
-import subprocess
 import argparse
 import glob
-
-
-
 
 parser = argparse.ArgumentParser(description ='Run uat_mask.py on all images that match input string')
 parser.add_argument('--string', dest = 'string', default = None, help = 'image string to match (e.g. A1367)')
@@ -48,4 +47,4 @@ input_images = glob.glob(search_string)
 
 for image in input_images:
     print 'masking image ',image
-    os.system('~/github/HalphaImaging/uat_mask.py --image '+image)
+    os.system(args.d+' --image '+image)
