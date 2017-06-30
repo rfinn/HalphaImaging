eb06324119a7057f16c08889167eb0fe3f38ac4f
Rose Finn <rfinn@coma.physics.siena.edu>
2017-06-30 13:55:47 -0400

    unfinished code

--- uat_measure_fwhm.py
+++ uat_measure_fwhm.py
@@ -13,13 +13,28 @@ parser.add_argument('--filestring', dest = 'filestring', default = 'hcftr*o00.ca
 args = parser.parse_args()
 
 input_cats = glob.glob(args.filestring)
-
-cat = fits.getdata(input_cats[2])
-'''
+input_cats.sort()
+#cat = fits.getdata(input_cats[2])
+print 'Image         mean (median)'
 for c in input_cats:
     # read in sextractor catalog
     # the catalog is in extension 2
     cat = fits.getdata(c,2)
-    plt.figure()
-    
-''' 
+    # select galaxies that are likely to be stars
+    # limit magnitude range so that stars are not saturated and not too faint
+    flag = (cat.MAG_AUTO > 10) &  (cat.FLAGS == 0) 
+    #flag = np.ones(len(cat.MAG_AUTO),'bool')
+    print '%20s: %5.1f (%5.1f)'%(c,np.mean(cat.FWHM_IMAGE[flag]),np.median(cat.FWHM_IMAGE[flag]))
+
+
+'''
+imstat hcftr7893t0061o00.fits nclip=3
+
+daofind hcftr7893t0062o00.fits
+
+psfmeasure hcftr7893t0062o00.fits imagecur =  hcftr7893t0062o00.fits.coo.2
+
+'''
+
+
+
