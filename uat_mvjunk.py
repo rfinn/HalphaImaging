6572e922172612be35be074f8b6f6b7184303b2e
rfinn@siena.edu <rfinn@siena.edu>
2018-05-31 13:21:22 -0400

    program to automatically move bad files

---
+++ uat_mvjunk.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+'''
+GOAL
+- make a subdirectory called junk (if it doesn't already exist)
+- move unwanted files to junk
+
+PROCEDURE
+- user creates a file called junkfiles with filenumbers to move
+- to create file, use 
+- example:
+0060-0068
+0074
+0100-0153
+
+USAGE:
+
+
+'''
+
+
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description ='Reads in images numbers and moves images to a subdirectory called junk.  File should contain 4 digit numbers that uniquely identify the image.  Ranges can be specified on one line using a dash.  For example:\n 0001 \n 0034-0042 \n')
+parser.add_argument('--filename', dest = 'filename', default = 'junkfiles', help = 'file containing list of image numbers to move to junk.  Default filename is junkfiles.')
+
+args = parser.parse_args()
+
+if not(os.path.exists('junk')):
+    os.mkdir('junk')
+
+infile = open(args.filename)
+for line in infile:
+    if line.find('-') > -1:
+        t = line.split('-')
+        os.system('mv *{'+t[0]+'..'+t[1]+'}*.fits junk/.')
+    else:
+        os.system('mv *'+line+'*.fits junk/.')
+
+infile.close()
