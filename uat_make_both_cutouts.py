e55deb3e1230c60e3052cef0dda11fc0f8918da6
rfinn@siena.edu <rfinn@siena.edu>
2018-06-11 15:16:59 -0400

    updating to make Ha and R cutouts at the same time

--- uat_make_both_cutouts.py
+++ uat_make_both_cutouts.py
@@ -17,23 +17,25 @@ INPUTS:
     catalog - catalog or subcatalog of NASA Sloan atlas
     halpha filter number - this is the filter number used to refer to the Halpha filter for your group/cluster (e.g. Ha8 would be 8)
 
-USAGE(this is more for the uat_make_cutouts code when not running both cutout filters at the same time):
+USAGE: 
 
 When running from macbook (not coma):
 
 ~/github/HalphaImaging/uat_make_cutouts.py --image A1367-h02_ha12.coadd.fits --catalog ~/research/NSA/nsa_v0_1_2.fits --filter Ha --nhalpha 12 --cluster A1367
 
-~/github/HalphaImaging/uat_make_cutouts.py --image A1367-h02_R.coadd.fits --catalog ~/research/NSA/nsa_v0_1_2.fits --filter R --nhalpha 12 --cluster A1367
+To run on Virgo filament imaging:
 
-~/github/HalphaImaging/uat_make_cutouts.py --image A1367-h02-CS.fits --catalog ~/research/NSA/nsa_v0_1_2.fits --filter CS --nhalpha 12 --cluster A1367
+~/github/HalphaImaging/uat_make_cutouts.py --image pointing-13_R.coadd.fits --catalog ~/github/Virgo/tables/nsa.virgo.fits --filter R --nhalpha 4 --prefix pointing13
 
-If running on coma, you don't need to specify the catalog path - the default should be the correct value.
+~/github/HalphaImaging/uat_make_cutouts.py --image pointing-13_ha4.coadd.fits --catalog ~/github/Virgo/tables/nsa.virgo.fits --filter ha4 --nhalpha 4 --prefix pointing13 --plot
 
-    uat_make_cutouts.py catalog image Halpha_filter_number
-
-
+%run ~/github/HalphaImaging/uat_make_both_cutouts.py --Rimage pointing-4_r.coadd.fits --Haimage pointing-4_ha4.coadd.fits --catalog ~/github/Virgo/tables/nsa.virgo.fits --filter1 R --filter2 Ha --nhalpha 4 --prefix pointing4 
 
 REQUIRED MODULES:
+
+astropy
+matplotlib
+numpy
 
 NOTES:
     You may need to change the variables Haimage, Rimage and Hawcimage if your image names are different
@@ -42,8 +44,6 @@ REFERENCES:
 
 http://docs.astropy.org/en/stable/nddata/utils.html
 
-EXAMPLE:
-%run ~/github/HalphaImaging/uat_make_both_cutouts.py --Rimage pointing-1_r.coadd.fits --Haimage pointing-1_ha4.coadd.fits --nhalpha 4 --prefix pointing-1
 
 """
 import numpy as np
@@ -68,16 +68,16 @@ from matplotlib.colors import LogNorm
 
 
 
-parser = argparse.ArgumentParser(description ='Get cutouts for NSA galaxies within field of view of mosaic and redshift range of designated H-alpha filter')
+parser = argparse.ArgumentParser(description ='Get cutouts for NSA galaxies within field of view of mosaic and redshift range of designated H-alpha filter \n example: \n %run ~/github/HalphaImaging/uat_make_both_cutouts.py --Rimage pointing-4_r.coadd.fits --Haimage pointing-4_ha4.coadd.fits --catalog ~/github/Virgo/tables/nsa.virgo.fits --filter1 R --filter2 Ha --nhalpha 4 --prefix pointing4  ')
 parser.add_argument('--Rimage', dest = 'Rimage', default = None, help = 'R-band HDI/mosaic image to make cutouts from')
 parser.add_argument('--Haimage', dest = 'Haimage', default = None, help = 'H-alpha HDI/mosaic image to make cutouts from')
 parser.add_argument('--catalog', dest = 'catalog', default = '/home/share/catalogs/nsa_v0_1_2.fits', help = 'full path to the NSA catalog')
 parser.add_argument('--filter1',dest = 'filter1', default ='R', help = 'Filter for the input mosaic image (e.g. r, R, Ha).  Default value is R.')
 parser.add_argument('--filter2',dest = 'filter2', default ='Ha', help = 'Filter for the input mosaic image (e.g. r, R, Ha).  Default value is Ha.')
-parser.add_argument('--nhalpha',dest = 'nhalpha', default ='12', help = 'H-alpha filter number (e.g. 4, 8, 12 or 16).  Default value is 12.')
+parser.add_argument('--nhalpha',dest = 'nhalpha', default ='4', help = 'H-alpha filter number (e.g. 4, 8, 12 or 16).  Default value is 4.')
 parser.add_argument('--Rscale',dest = 'scale', default =16., help = 'cutout size = (scale x Re, scale x Re) - increase scale to increase size of cutout.  Default value is 16.')
 parser.add_argument('--prefix',dest = 'prefix', default ='A1367', help = 'cluster name to preprend to cutout image names (no spaces!).  Default value is A1367.')
-parser.add_argument('--plot', dest = 'plot', default = False, action = 'store_true', help = 'plot cutouts and position wrt mosaic.  Default value is False.')
+parser.add_argument('--plot', dest = 'plot', default = False, action = 'store_true', help = "add '--plot ' to plot cutouts and position wrt mosaic.  Default value is False.")
 
 
 args = parser.parse_args()
@@ -175,15 +175,14 @@ def makebothcuts(Rimage,filter1,Haimage,filter2):
         newfile1.header.update(w[ymin1:ymax1,xmin1:xmax1].to_header())
         
         fits.writeto(outimage1, newfile1.data, header = newfile1.header, clobber=True)
-    return cutoutR
-    return cutoutHa
-           # plt.figure()
-           # plt.imshow(g[0].data, origin='lower',cmap='gray', norm=LogNorm())
-           # cutoutHa.plot_on_original(color='white')
-           # plt.show()
-           # r = raw_input('type any key to continue (p to skip plotting) \n')
-           # if r.find('p') > -1:
-            #    args.plot = False
+        if args.plot:
+           plt.figure()
+           plt.imshow(g[0].data, origin='lower',cmap='gray', norm=LogNorm())
+           cutoutHa.plot_on_original(color='white')
+           plt.show()
+           r = raw_input('type any key to continue (p to skip plotting) \n')
+           if r.find('p') > -1:
+                args.plot = False
         # figure out how to save the cutout as fits image
         #((ymin,ymax),(xmin,xmax)) = cutoutHa.bbox_original
        # outimage = args.prefix+'-'+(str(IDNUMBER[i])+'-'+ args.filter2+".fits")
@@ -195,125 +194,5 @@ def makebothcuts(Rimage,filter1,Haimage,filter2):
        # fits.writeto(outimage, newfile.data, header = newfile.header, clobber=True)
 if __name__ == '__main__':
     imcutout1 = makebothcuts(args.Rimage,args.filter1,args.Haimage,args.filter2)
-    imcutout2 = makebothcuts(args.Rimage,args.filter1,args.Haimage,args.filter2)
-
-#NATASHA NEW STUFF TO RUN BOTH:
-#I want to use the known argument Filter to run both --filter R and --filter Ha at the same time, so it can display Ha, then R.
-#Or add another argument, filter 2, which will run after filter argument, but in same command
-
-
-
-#Need to define Rimage, Haimage
-#def bothcutouts(Rimage, Haimage, 
-
-
-########## OLD STUFF ############
-
-#Name of images
-Haimage=('Hacs_final.fits')
-Rimage=('R_final.fits')
-Hawcimage=('Ha_final.fits')
-#Hawc is image with continuum 
-
-
-def get_cutout(image,RA,Dec,size):
-    print 'getting cutouts!'
-
-
-def cutout(filename, xc, yc, xw=25, yw=25, units='pixels', outfile=None,
-        clobber=True, useMontage=False, coordsys='celestial', verbose=False):
-    
-    #Inputs:
-        #file  - .fits filename or pyfits HDUList (must be 2D)
-        #xc,yc - x and y coordinates in the fits files' coordinate system (CTYPE)
-        #xw,yw - x and y width (pixels or wcs)
-        #units - specify units to use: either pixels or wcs
-        #outfile - optional output file
-    
-
-    if isinstance(filename,str):
-        file = pyfits.open(filename)
-        opened=True
-    elif isinstance(filename,pyfits.HDUList):
-        file = filename
-        opened=False
-    else:
-        raise Exception("cutout: Input file is wrong type (string or HDUList are acceptable).")
-
-    head = file[0].header.copy()
-
-    if head['NAXIS'] > 2:
-        raise DimensionError("Too many (%i) dimensions!" % head['NAXIS'])
-    cd1 = head.get('CDELT1') if head.get('CDELT1') else head.get('CD1_1')
-    cd2 = head.get('CDELT2') if head.get('CDELT2') else head.get('CD2_2')
-    if cd1 is None or cd2 is None:
-        raise Exception("Missing CD or CDELT keywords in header")
-    wcs = pywcs.WCS(head)
-
-    if units == 'wcs':
-        if coordsys=='celestial' and wcs.wcs.lngtyp=='GLON':
-            xc,yc = coords.Position((xc,yc),system=coordsys).galactic()
-        elif coordsys=='galactic' and wcs.wcs.lngtyp=='RA':
-            xc,yc = coords.Position((xc,yc),system=coordsys).j2000()
-
-    if useMontage and CanUseMontage:
-        head['CRVAL1'] = xc
-        head['CRVAL2'] = yc
-        if units == 'pixels':
-            head['CRPIX1'] = xw
-            head['CRPIX2'] = yw
-            head['NAXIS1'] = int(xw*2)
-            head['NAXIS2'] = int(yw*2)
-        elif units == 'wcs':
-            
-            cdelt = numpy.sqrt(cd1**2+cd2**2)
-            head['CRPIX1'] = xw   / cdelt
-            head['CRPIX2'] = yw   / cdelt
-            head['NAXIS1'] = int(xw*2 / cdelt)
-            head['NAXIS2'] = int(yw*2 / cdelt)
-
-        head.toTxtFile('temp_montage.hdr',clobber=True)
-        newfile = montage.wrappers.reproject_hdu(file[0],header='temp_montage.hdr',exact_size=True)
-        os.remove('temp_montage.hdr')
-    else:
-
-        xx,yy = wcs.wcs_world2pix(xc,yc,0)
-        
-        if units=='pixels':
-            xmin,xmax = numpy.max([0,xx-xw]),numpy.min([head['NAXIS1'],xx+xw])
-            ymin,ymax = numpy.max([0,yy-yw]),numpy.min([head['NAXIS2'],yy+yw])
-        elif units=='wcs':
-            xmin,xmax = numpy.max([0,xx-xw/numpy.abs(cd1)]),numpy.min([head['NAXIS1'],xx+xw/numpy.abs(cd1)])
-            ymin,ymax = numpy.max([0,yy-yw/numpy.abs(cd2)]),numpy.min([head['NAXIS2'],yy+yw/numpy.abs(cd2)])
-        else:
-            raise Exception("Can't use units %s." % units)
-
-        if xmax < 0 or ymax < 0:
-            raise ValueError("Max Coordinate is outside of map: %f,%f." % (xmax,ymax))
-        if ymin >= head.get('NAXIS2') or xmin >= head.get('NAXIS1'):
-            raise ValueError("Min Coordinate is outside of map: %f,%f." % (xmin,ymin))
-
-
-        head['CRPIX1']-=xmin
-        head['CRPIX2']-=ymin
-        head['NAXIS1']=int(xmax-xmin)
-        head['NAXIS2']=int(ymax-ymin)
-
-        if head.get('NAXIS1') == 0 or head.get('NAXIS2') == 0:
-            raise ValueError("Map has a 0 dimension: %i,%i." % (head.get('NAXIS1'),head.get('NAXIS2')))
-
-        img = file[0].data[ymin:ymax,xmin:xmax]
-        newfile = pyfits.PrimaryHDU(data=img,header=head)
-        if verbose: print "Cut image %s with dims %s to %s.  xrange: %f:%f, yrange: %f:%f" % (filename, file[0].data.shape,img.shape,xmin,xmax,ymin,ymax)
-
-    if isinstance(outfile,str):
-        newfile.writeto(outfile,clobber=clobber)
-
-    if opened:
-        file.close()
-
-    return newfile  
-#End of galaxy cutout
-
-
+    #imcutout2 = makebothcuts(args.Rimage,args.filter1,args.Haimage,args.filter2)
 
