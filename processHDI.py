7d99db3f435e1689edc2ef8d871530b90e10e383
rfinn@siena.edu <rfinn@siena.edu>
2019-10-01 18:25:01 -0400

    option for stages

--- processHDI.py
+++ processHDI.py
@@ -21,34 +21,48 @@ sys.path.append('~/github/HalphaImaging/')
 current_dir = os.getcwd()
 # trim and overscan correct
 
-os.system('python '+gitpath+'uat_overscantrim.py --filestring c')
+trim = False
+zap = False
+group_flat = False
+dflat = True
+fixheader=True
+astr = True
+
+if trim:
+    os.system('python '+gitpath+'uat_overscantrim.py --filestring c')
 os.mkdir('ORIGINALS')
 os.system('mv c*.fits ORIGINALS/.')
+
 # zap cosmic rays
-os.system('python '+gitpath+'uat_zapcosmicrays.py --filestring tr')
+if zap:
+    os.system('python '+gitpath+'uat_zapcosmicrays.py --filestring tr')
 os.mkdir('TRIMMED')
 os.system('mv tr*.fits TRIMMED/.')
 
 # group flat files
-os.system('python '+gitpath+'uat_HDIgroupflatfiles.py --filestring ztr')
+if group_flat:
+    os.system('python '+gitpath+'uat_HDIgroupflatfiles.py --filestring ztr')
 
 # flatten science frames with dome flats
-os.system('python '+gitpath+'uat_HDIflattendwithdome.py --filestring ztr')
-os.mkdir('ZAPPED')
-os.system('mv z*.fits ZAPPED/.')
+if dflat:
+    os.system('python '+gitpath+'uat_HDIflattenwithdome.py --filestring ztr')
+    os.mkdir('ZAPPED')
+    os.system('mv z*.fits ZAPPED/.')
 
 
 # fix the HDI header
-os.system('python '+gitpath+' uat_HDIfixheader.py --filestring d')
+if fixheader:
+    os.system('python '+gitpath+' uat_HDIfixheader.py --filestring d')
 
 # run sextractor to create object lists
-os.system('python '+gitpath+'uat_astr_mosaic.py --s --filestring h')
+if astr:
+    os.system('python '+gitpath+'uat_astr_mosaic.py --s --filestring h')
 
-# run scamp to solve for astrometry
-os.system('python '+gitpath+'uat_astr_mosaic.py --scamp --filestring h')
+    # run scamp to solve for astrometry
+    os.system('python '+gitpath+'uat_astr_mosaic.py --scamp --filestring h')
 
-# sort objects by field
-os.system('python '+gitpath+'uat_HDIsortobjects.py --filestring h')
+    # sort objects by field
+    os.system('python '+gitpath+'uat_HDIsortobjects.py --filestring h')
 
 # run swarp?
 #os.system('python '+gitpath+'uat_HDIsortobjects.py --filestring h')
