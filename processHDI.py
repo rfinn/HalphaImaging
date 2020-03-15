71666b1b1d0588777121c490bfc75db241877bbd
rfinn <rfinn>
2020-03-15 17:35:17 -0400

    updates

--- processHDI.py
+++ processHDI.py
@@ -107,11 +107,24 @@ if astr:
 #os.system('python '+gitpath+'uat_HDIsortobjects.py --filestring h')
 if args.swarp:
     infile = open(args.filelist,'r')
+    print('running swarp')
+    i = 0
     for f in infile:
         f = f.rstrip()
-        rootname = f.split('_R')[0]
+        print(f)
+        if f.find(r'_R') > -1:
+            print('R filter')
+            rootname = f.split('_R')[0]
+        elif f.find(r'_r') > -1:
+            print('sdss r filter')
+            rootname = f.split('_r')[0]
+        else:
+            print('unexpected file name format')
+            sys.exit()
         # get set of halpha images
+        print('rootname = ',rootname)
         fnames = glob.glob(rootname+'_h*')
+        print('halpha file = ',fnames)
         if len(fnames) > 1:
             print('got more than one Halpha image - crazy!')
             print('hope this is ok...')
@@ -135,7 +148,8 @@ if args.swarp:
             os.system('python '+gitpath+'uat_astr_mosaic.py --swarp --l '+halist+' --refimage '+rcoadd_image)
         # run swarp on r, with r as reference image
         os.system('python '+gitpath+'uat_astr_mosaic.py --swarp --l '+f+' --refimage '+rcoadd_image)
-        #break
+        break
+        
     infile.close()
 
 # solve for photometric zp
