08011a607e7e81de85d1790aaa0997d06bddbbc3
rfinn <rfinn>
2021-03-05 15:28:31 -0500

    subtract sky

--- python3/imutils.py
+++ python3/imutils.py
@@ -20,7 +20,7 @@ import numpy as np
 def subtract_median_sky(data):
     ''' subtract median sky from image data '''
     mask = make_source_mask(data,nsigma=3,npixels=5,dilate_size=10)
-    masked_data = np.ma(data,mask=mask)
+    masked_data = np.ma.array(data,mask=mask)
     #clipped_array = sigma_clip(masked_data,cenfunc=np.ma.mean)
 
     mean,median,std = sigma_clipped_stats(masked_data,sigma=3.0,cenfunc=np.ma.mean)
