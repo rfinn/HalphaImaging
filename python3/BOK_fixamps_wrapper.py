8efe6dc95c61901a6974144f9472197218d11e41
rfinn <rfinn>
2023-05-31 23:18:22 -0400

    updates for 90prime

--- python3/BOK_fixamps_wrapper.py
+++ python3/BOK_fixamps_wrapper.py
@@ -20,7 +20,7 @@ def processone(image):
     
     #image = filelist[i]
     outimage = 'zm'+image
-    print("processing ",image)
+    #print("processing ",image)
     #if os.path.exists(outimage):
     #    print(f'{outimage} found - moving to next object')
     #else:
