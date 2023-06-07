b175b39e1a631934184e1e06ecbdda2ab2d7f768
rfinn <rfinn@siena.edu>
2023-06-07 16:33:11 -0400

    program to identify fixamps failures

--- python3/get_failed_fixamps.py
+++ python3/get_failed_fixamps.py
@@ -9,10 +9,16 @@ import os
 
 mfiles = glob.glob('mksb*ooi*v1.fits')
 mfiles.sort()
+outdir = 'fixamp-failures'
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+    
 for m in mfiles:
     zfile = 'z'+m
     hfile = m.replace('.fits','.head')
     if not os.path.exists(zfile) and os.path.exists(hfile):
         print(m)
+        os.rename(m,outdir+'/'+m)
     elif not os.path.exists(zfile) and not os.path.exists(hfile):
         print(f"{m} missing {hfile}")
+    
