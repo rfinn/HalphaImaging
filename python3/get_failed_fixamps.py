789e9d929feb88318a6179f14d18c29f540d3f3f
rfinn <rfinn@siena.edu>
2023-06-07 16:20:11 -0400

    program to identify fixamps failures

---
+++ python3/get_failed_fixamps.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+GOAL: print out files that have sky subtraction but fixamps failed.
+
+"""
+import glob
+import os
+
+mfiles = glob.glob('mksb*ooi*v1.fits')
+
+for m in mfiles:
+    zfile = 'z'+m
+    if not os.path.exists(zfile):
+        print(m)
