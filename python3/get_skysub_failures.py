207d262c742e1b28b0fd43d375e4e4912a48071b
rfinn <rfinn>
2023-06-25 14:53:27 -0400

    getting images with failed sky subtraction

--- python3/get_skysub_failures.py
+++ python3/get_skysub_failures.py
@@ -14,7 +14,7 @@ if not os.path.exists(outdir):
     os.mkdir(outdir)
     
 for k in kfiles:
-    zfile = 'm'+k
+    mfile = 'm'+k
     hfile = k.replace('.fits','.head')
     if not os.path.exists(mfile) and os.path.exists(hfile):
         print(m)
