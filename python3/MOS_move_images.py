5165b89c887f3a29f1f684f7d0305277e6537195
rfinn <rfinn@siena.edu>
2023-07-06 16:14:28 -0400

    code to move images to final directory

---
+++ python3/MOS_move_images.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+move fVF*.fits from /data-pool/Halpha/coadds/virgo-coadds-MOSAIC/ to VF*.fits in /data-pool/Halpha/coadds/all-virgo-coadds/
+
+"""
+import shutil
+import glob
+
+indir = '/data-pool/Halpha/coadds/virgo-coadds-MOSAIC/'
+outdir = '/data-pool/Halpha/coadds/all-virgo-coadds/'
+
+flist = glob.glob(indir+'fVF*.fits')
+flist.sort()
+
+for f in flist:
+    outname = f.replace('fVF','VF')
+    print(f"{f} -> {outname}")
+    #shutil.copyfile(f,outname)
