ed3efe17e70e31cd43d94a0be0828c545c023ba4
rfinn <rfinn>
2023-07-11 13:44:39 -0400

    updating for running in parallel on draco

--- python3/generate_all_cutout_plots.py
+++ python3/generate_all_cutout_plots.py
@@ -12,15 +12,11 @@ NOTES:
 """
 #import glob
 import os
-import multiprocessing as mp
+
 import sys
 homedir = os.getenv('HOME')
 sys.path.append(homedir+'/github/HalphaImaging/python3/')
 
-dirlist = glob.glob('VFID*')
-dirlist.sort()
-workingdir = os.getcwd()
-dirlist = [f.path for f in os.scandir(workingdir) if f.is_dir() ]
 def getone(d):
     imlist = glob.glob(d+'*-R.fits')
     if len(imlist) == 0:
@@ -30,15 +26,37 @@ def getone(d):
         
     for im in imlist:
         os.system('python '+homedir+'/github/HalphaImaging/python3/plot_cutouts_ha.py --r '+im+' --plotall')
-    os.chdir(workingdir)    
-for d in dirlist:
-    image_pool = mp.Pool(mp.cpu_count())
-    #image_pool = mp.pool.ThreadPool(mp.cpu_count())    
-    myresults = [image_pool.apply_async(buildone,args=(d,)) for d in dirlist]
+    os.chdir(workingdir)
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description ='get cutout images for all cutouts.  Run this from the directory that contains subdirectories for each galaxy.  For example: /data-pool/Halpha/halphagui-output-20230711/')
+    parser.add_argument('--onegal',dest = 'onegal', default=None, help = "use this to run on one image only.  Specify the full path to the r-band image. ")
+    args = parser.parse_args()
+
+
+
+    if args.onegal is not None:
+        # run on one image only
+        # this is used for running in parallel
+        getone(args.onegal)
+    else:
+        import multiprocessing as mp
+        dirlist = glob.glob('VFID*')
+        dirlist.sort()
+        workingdir = os.getcwd()
+        dirlist = [f.path for f in os.scandir(workingdir) if f.is_dir() ]
+        
+
     
-    image_pool.close()
-    image_pool.join()
-    image_results = [r.get() for r in myresults]
+        for d in dirlist:
+            image_pool = mp.Pool(mp.cpu_count())
+            #image_pool = mp.pool.ThreadPool(mp.cpu_count())    
+            myresults = [image_pool.apply_async(buildone,args=(d,)) for d in dirlist]
+            
+            image_pool.close()
+            image_pool.join()
+            image_results = [r.get() for r in myresults]
     
         
 
