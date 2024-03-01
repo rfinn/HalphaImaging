90440219e848f72907c6a719f5855d6684423fe7
rfinn <rfinn>
2024-03-01 16:30:10 -0500

    catching different variations in filter naming conventions

--- python3/uat_HDIflattenwithdome.py
+++ python3/uat_HDIflattenwithdome.py
@@ -59,14 +59,13 @@ for line in infile:
     t=line.split('.fits')
     filenames.append(t[0]+'.fits')
     if (line.find('6620') > -1) | (t[1].find('ha4') > -1):
-        filter.append('ha4')
-
+        filefilter.append('ha4')
     elif (line.find('6660') > -1) |(line.find('ha8') > -1) :
-        filter.append('ha8')
+        filefilter.append('ha8')
     elif (line.find('6700') > -1) |(line.find('ha12') > -1) :
-        filter.append('ha12')
+        filefilter.append('ha12')
     elif (line.find('6740') > -1) |(line.find('ha16') > -1) :
-        filter.append('ha16')
+        filefilter.append('ha16')
     
     elif t[1].find('R') > -1:
         filefilter.append('R')
