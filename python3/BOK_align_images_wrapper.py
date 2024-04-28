74a89850ebdf48d662a7d11bbd94186fc5fa4e02
rfinn <rfinn>
2024-04-28 17:42:29 -0400

    some issues with images where ha4 and r have different dates in image name...

--- python3/BOK_align_images_wrapper.py
+++ python3/BOK_align_images_wrapper.py
@@ -33,12 +33,34 @@ parallel --eta python ~/github/HalphaImaging/python3/BOK_align_images_wrapper.py
 """
 import os
 import sys
+import glob
 
 himage = sys.argv[1]
 rimage = himage.replace('Ha4.fits','r.fits')
 weightimage = rimage.replace('r.fits','r.weight.fits')
 
 rshifted = rimage.replace('r.fits','r-shifted.fits')
+if not os.path.exists(rimage):
+    print("warning: default r image name is not working for ",himage, rimage)
+    t = himage.split('-')
+    ######################################################
+    # check to see if r-band was taken on a different date
+    ######################################################
+
+    # get date from filestring
+    obsdate = t[-3]
+
+    # replace date with *
+    search_name = rimage.replace(obsdate,'*')
+
+    # look for files using glob
+    filelist = glob.glob(search_name)
+
+    # if a match was found, redefine names
+    if len(filelist) > 0:
+        rimage = filelist[0]
+        weightimage = rimage.replace('r.fits','r.weight.fits')
+        rshifted = rimage.replace('r.fits','r-shifted.fits')
 if os.path.exists(rshifted):
     print("shifted image found - skipping ",himage)
     sys.exit()
