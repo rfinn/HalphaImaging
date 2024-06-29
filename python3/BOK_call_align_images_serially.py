7fe65d84a16ea99ab594d606dc1dc00ea83c29f2
rfinn <rfinn>
2024-06-29 14:13:38 -0400

    trying to resolve alignment issue with BOK

--- python3/BOK_call_align_images_serially.py
+++ python3/BOK_call_align_images_serially.py
@@ -15,13 +15,19 @@ import os
 
 filelist = sys.argv[1]
 
-infiles = open(filelist,'r')
+# get number of lines
+t = open(filelist,'r')
+nlines = len(t.readlines())
+t.close()
 
+
+infiles = open(filelist,'r')
 for i,line in enumerate(infiles):
     print()
-    print(f"good news - running {i+1} out of {len(infiles)}")
+    print(f"good news - running {i+1} out of {nlines}")
     t = line.rstrip()
     print(t)
     s = f"python ~/github/HalphaImaging/python3/BOK_align_images_wrapper.py {t}"
     os.system(s)
-    
+
+infiles.close()
