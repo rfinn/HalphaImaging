77a43dd319164b7f13ac87a77e3b5283723637fa
rfinn <rfinn@siena.edu>
2025-09-04 15:22:46 -0400

    - working on #51. - bok files don't seem to work when running in parallel.  Making this program to run the files in serials.

---
+++ python3/batch_getzp_bok.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+"""
+* want to rerun getzp on all coadds b/c I've change how I am fitting ZP
+* I have incorporated color correction from Matteo Fossati
+
+* want to run all again to make sure all is done in a uniform way for all coadds
+
+* this assumes that the coadds have the most recent naming convention, which includes the telescope and filter
+
+USAGE:
+
+from /media/rfinn/hdata/coadds/all-virgo-coadds
+
+python ~/github/HalphaImaging/python3/batch_getzp.py
+
+To run on a subset of files, give a match string VF*matchstring*.fits:
+
+  python ~/github/HalphaImaging/python3/batch_getzp.py BOK
+
+"""
+import glob
+import os
+import sys
+import numpy as np
+import multiprocessing as mp
+
+image_results = []
+def collect_results(result):
+
+    global results
+    image_results.append(result)
+
+
+    
+def runone(i,f):
+    if f.find('weight') > -1:
+        return
+
+    # skip CS images
+    if f.find('CS.fits') > -1:
+        return
+    if f.find('CS-ZP.fits') > -1:
+        return
+    # how do we skip the unshifted files?
+    # this only seems to apply to INT data (why did I need to shift these, anyway???)
+    # so if file has INT and -r
+    if ('INT' in f) and ('r.fits' in f):
+        return
+    
+    if ('BOK' in f) and ('r.fits' in f):
+        return
+    
+    # determine telescope and filter from filename
+    t = f.split('-')
+    #print(t)
+    # check to see if declination is negative
+    # if it is, then index of instrument will be off by one
+    if f[10] == '+':
+        instrument = t[2]
+        filter = t[5].replace('.fits','')
+    else:
+        instrument = t[3]
+        filter = t[6].replace('.fits','')
+        
+
+    halpha_names = ['ha4','Halpha','Ha6657','Ha4']
+    if filter in halpha_names:
+        ffilter = 'ha'
+    elif filter == 'r':
+        ffilter = 'r'
+    elif filter == 'R':
+        ffilter = 'R'
+    else:
+        print('WARNING: did not recognize filter ',filter, f)
+        sys.exit()
+
+    # get instrument
+    if instrument == 'INT':
+        iinstrument = 'i'
+    elif instrument == 'BOK':
+        iinstrument = 'b'
+    elif instrument == 'HDI':
+        iinstrument = 'h'
+    elif instrument == 'MOSAIC':
+        iinstrument = 'm'
+    elif instrument == 'MOS': # didn't have this case for mosaic images
+        iinstrument = 'm'
+    else:
+        print('WARNING: did not recognize instrument ',instrument, f)
+        sys.exit()
+    
+    # construct string
+    getzpstring = f"python ~/github/HalphaImaging/python3/getzp.py --image {f} --instrument {iinstrument} --filter {ffilter}"
+    #if instrument == 'BOK':
+    #    getzpstring += ' --fixbok'
+    print()
+    print(f"Running getzp.py for file {f}")
+    #print(getzpstring)
+
+    # run getzp
+    os.system(getzpstring)
+
+# run one galaxy at a time, in parallel
+    # don't need this if I am going to run using gnu parallel instead
+if len(sys.argv) > 1:
+    matchstring = sys.argv[1]
+    rfiles = glob.glob('VF*'+matchstring+'*r-shifted.fits') # grab subset of files in the directory
+    for i,f in enumerate(rfiles):
+        runone(i,f)
+        
+
+    
+
+
