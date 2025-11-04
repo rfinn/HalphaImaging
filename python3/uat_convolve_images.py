3445c59d364c64aac26e9d0ab48bcd85e831ba5a
rfinn <rfinn@siena.edu>
2025-11-04 15:54:43 -0500

    - working on #54. - trying to improve output printing.  and added mag selection back into get_fwhm - trying to fix nan that are coming back from get_fwhm using np.nanmedian - cleaning up files that psfex creates - fixed typo - adding another estimate of output fwhm - fixed a typo in filename - fixed error in naming gauss_list - fixed error in naming of psfex output files that we are cleaning up. - using median rather than mean to estimate the image FWHM in the function get_fwhm - maybe psfex output files don't have the 'g' prefix added

--- python3/uat_convolve_images.py
+++ python3/uat_convolve_images.py
@@ -62,7 +62,7 @@ def get_fwhm(input_images): #measure FWHM of SE catalogs
         se_cat = t[0]+'.cat'
         data = fits.getdata(se_cat,2)
         # select unsaturated stars using : class_star > 0.9 and (10 < m < 13)
-        flag = (data.CLASS_STAR > 0.9) #& (data.MAG_AUTO > 10.) & (data.MAG_AUTO < 13.)
+        flag = (data.CLASS_STAR > 0.9) & (data.MAG_AUTO > 10.) & (data.MAG_AUTO < 13.)
         image_fwhm[i] = np.nanmedian(data.FWHM_IMAGE[flag])
         image_fwhm_std[i] = np.std(data.FWHM_IMAGE[flag])
     return image_fwhm, image_fwhm_std
@@ -222,7 +222,7 @@ if __name__ == '__main__':
         print('#######################################')
         print('COMPARING ORIGINAL AND FINAL FWHM')
         print('#######################################')
-        print("image   fwhm    gfwhm(auto)  gfwhm(manual)")
+        print("image            fwhm  gfwhm(auto) gfwhm(man)")
         print("------------------------")
         for i in range(len(image_names)):
             print(f"{image_names[i]}: {image_fwhm[i]:.2f} {gimage_fwhm[i]:.2f} {myimage_fwhm[i]:.2f} ratio={gimage_fwhm[i]/fwhm_max:.3f}")
