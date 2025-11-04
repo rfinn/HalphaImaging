05f0648a1bb21dc77b76a58d4ea74430f606f9d4
rfinn <rfinn@siena.edu>
2025-11-04 15:49:54 -0500

    - working on #54. - trying to fix nan that are coming back from get_fwhm using np.nanmedian - cleaning up files that psfex creates - fixed typo - adding another estimate of output fwhm - fixed a typo in filename - fixed error in naming gauss_list - fixed error in naming of psfex output files that we are cleaning up. - using median rather than mean to estimate the image FWHM in the function get_fwhm - maybe psfex output files don't have the 'g' prefix added

--- python3/uat_convolve_images.py
+++ python3/uat_convolve_images.py
@@ -62,8 +62,8 @@ def get_fwhm(input_images): #measure FWHM of SE catalogs
         se_cat = t[0]+'.cat'
         data = fits.getdata(se_cat,2)
         # select unsaturated stars using : class_star > 0.9 and (10 < m < 13)
-        flag = (data.CLASS_STAR > 0.9) & (data.MAG_AUTO > 10.) & (data.MAG_AUTO < 13.)
-        image_fwhm[i] = np.median(data.FWHM_IMAGE[flag])
+        flag = (data.CLASS_STAR > 0.9) #& (data.MAG_AUTO > 10.) & (data.MAG_AUTO < 13.)
+        image_fwhm[i] = np.nanmedian(data.FWHM_IMAGE[flag])
         image_fwhm_std[i] = np.std(data.FWHM_IMAGE[flag])
     return image_fwhm, image_fwhm_std
 
@@ -112,14 +112,13 @@ if __name__ == '__main__':
 
 
     ######################################################
-    ## RUN SOURCE EXTRACTOR USING THE CORRECT CONFIG FILE
+    ## GET THE LIST OF SOURCE EXTRACTOR CATALOGS
     ######################################################
     if args.hdi:
         os.system('ls '+args.filestring+'*o00.cat > input_list')
     else:
         # removing o00 to make this compatible with mosaic data
         os.system('ls '+args.filestring+'*.cat > input_list')
-
 
     
 
