69e89df6a6a31c036aeb7acd383560a3afccd6a8
rfinn <rfinn@siena.edu>
2025-12-07 13:50:48 -0500

    working on #64 - fixing typo - skipping swarp, for debugging purposes...

--- python3/BOK_run_swarp.py
+++ python3/BOK_run_swarp.py
@@ -301,7 +301,7 @@ def run_swarp(image_list,refimage=None, verbose=True):
     print('')
     print('Running swarp with the following command:\n',commandstring)
     # skip actually running swarp, just to check the output commands
-    #os.system(commandstring)
+    os.system(commandstring)
 
 
 
