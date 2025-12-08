ddc8296a4185d80b5bff2630a524181ccbdacfa6
rfinn <rfinn@siena.edu>
2025-12-08 12:16:36 -0500

    working on #66 - incorporating in and out directories - fixing typos... - skipping weight files and CS-ZP files in the main loop.  CS-ZP should be made AFTER renaming coadds. - setting this up first for HDI coadds.  will need to test mosaic names separately

--- python3/uat_rename_coadds.py
+++ python3/uat_rename_coadds.py
@@ -50,15 +50,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description ='stack the 90prime images after noao pipeline')
     parser.add_argument('--filestring', dest = 'filestring', default = 'coadd', help = 'filestring to match. default is coadd, which will grab all *coadd*.fits.  Note: weight images will be renamed with their corresponding science coadd.')
         
-    parser.add_argument('--indir', dest = 'indir', default = '.', help = 'directory of input images.  default is current directory')
-    parser.add_argument('--outdir', dest = 'outdir', default = '.', help = 'directory to write output images to.  default is current directory')
+    parser.add_argument('--indir', dest = 'indir', default = '.', help = 'directory of input images.  Default is current directory')
+    parser.add_argument('--outdir', dest = 'outdir', default = '.', help = 'directory to write output images to.  Default is current directory')
+    parser.add_argument('--testing', dest = 'testing', default = False, action='store_true', help = 'new filenames are printed when this is set, but the files are not renamed.  This is useful when testing changes to the code.')    
 
     
     args = parser.parse_args()
 
 
     # grab all the coadds in a particular directory
-    filelist = glob.glob("*"+args.filestring+"*.fits")
+    filelist = glob.glob(os.path.join(args.indir,"*"+args.filestring+"*.fits"))
     filelist.sort()
 
 
@@ -67,16 +68,19 @@ if __name__ == '__main__':
             continue
         if 'CS-ZP' in fname:
             continue
-        new_output_image = get_updated_uat_coadd_name(fname)
+        new_name = get_updated_uat_coadd_name(os.path.basename(fname))
         # rename as UAT-{RA}-{DEC}-{TEL}-{OBSDATE}-{POINTING}-{FILTER}
+        new_output_image = os.path.join(args.outdir, new_name)
         print('renaming ',fname,'->',new_output_image)
-        #os.rename(fname,new_output_image)
+        if not args.testing:
+            os.rename(fname,new_output_image)
 
         # rename the weight file
         weightfile = fname.replace('.fits','.weight.fits')
         if os.path.exists(weightfile):
             print('renaming ',weightfile,'->',new_output_image.replace('.fits','.weight.fits'))
-            #os.rename(weightfile,new_output_image.replace('.fits','.weight.fits'))
+            if not args.testing:
+                os.rename(weightfile,new_output_image.replace('.fits','.weight.fits'))
         else:
             print("WARNING: weight image does not exist for ",fname)
 
