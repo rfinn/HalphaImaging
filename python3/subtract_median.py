b9b8c639d45a6d81355994776f746796fbcdda34
rfinn <rfinn@siena.edu>
2025-12-10 14:45:51 -0500

    working on #61 - added a catch in case amplifiers are missing on the input mosaic image, as what the case with some images in a sequence.

--- python3/subtract_median.py
+++ python3/subtract_median.py
@@ -149,20 +149,29 @@ def subtract_median_one(fname,MEF=True,overwrite=False,MOS=False):
                 print("\nworking on namp ",namp)
                 ymin = yvals[k]
                 ymax = yvals[k+1]
-            
+
+
                 d,median,std = imutils.subtract_median_sky(hdu[0].data[ymin+buffer:ymax-buffer,xmin+buffer:xmax-buffer],getstd=True,subtract=False,\
                                                                weightimage = whdu[0].data[ymin+buffer:ymax-buffer,xmin+buffer:xmax-buffer])
 
-                average_med += median
-                average_std += std
-                # subtract median
+                if math.isnan(median):
+                    median = 'NAN'
+                    std = 'NAN'
+                    
+                else:
 
-                hdu[0].header.set(f'REGION{namp:02d}',value=f"{xmin}:{xmax},{ymin}:{ymax}",comment='{xmin}:{xmax},{ymin}:{ymax}')                               
+                    average_med += median
+                    average_std += std
+
+                    hdu[0].data[ymin:ymax,xmin:xmax][good_mask[ymin:ymax,xmin:xmax]] -= median                    
+                    # subtract median
+                    
+                hdu[0].header.set(f'REGION{namp:02d}',value=f"{xmin}:{xmax},{ymin}:{ymax}",comment='{xmin}:{xmax},{ymin}:{ymax}')                
                 hdu[0].header.set(f'MEDSUB{namp:02d}',value=median,comment='sky med subtract_med')                
                 hdu[0].header.set(f'SKYMED{namp:02d}',value=median,comment='sky med subtract_med')
                 hdu[0].header.set(f'SKYSTD{namp:02d}',value=std,comment='sky std subtract_med')
 
-                hdu[0].data[ymin:ymax,xmin:xmax][good_mask[ymin:ymax,xmin:xmax]] -= median
+                
                 
                 namp += 1
         # calculate average med and std
