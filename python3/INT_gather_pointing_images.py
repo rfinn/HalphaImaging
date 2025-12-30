9d024c796886e29cfed585b066098bd5cd5cceaf
rfinn <rfinn@siena.edu>
2025-12-30 18:24:36 -0500

    - working on #69 - testing code to gather all images for a particular pointing into one directory

--- python3/INT_gather_pointing_images.py
+++ python3/INT_gather_pointing_images.py
@@ -14,41 +14,73 @@ import os
 import shutil
 from astropy.io import fits
 import glob
+import argparse
 
-# get list of current directory
-flist1 = os.listdir()
-flist1.sort()
+if __name__ == '__main__':
 
-# get list of pointings - cut off filter from dir name
-pointings = []
-for f in flist1:
-    if( (f.find('pointing') > -1) | (f.find('target') > -1)) & (f.find('-') > -1):
-        pointings.append(f.split('-')[0])
+    
+    parser = argparse.ArgumentParser(description ="This program will combine images from e.g. pointing001-r and pointing001-Halpha into pointing001 and remove the filter-based directories.")
+    #parser.add_argument('--filestring', dest = 'filestring', default = 'coadd', help = 'filestring to match. default is coadd, which will grab all *coadd*.fits.  Note: weight images will be renamed with their corresponding science coadd.')
+        
+    #parser.add_argument('--indir', dest = 'indir', default = '.', help = 'directory of input images.  Default is current directory')
+    #parser.add_argument('--outdir', dest = 'outdir', default = '.', help = 'directory to write output images to.  Default is current directory')
+    parser.add_argument('--testing', dest = 'testing', default = False, action='store_true', help = 'Commands are printed when this is set, but the files are not moved.  This is useful when testing changes to the code.')    
 
-# pointings will now have two listing for each filter b/c of r and Halpha
-upointings = set(pointings) # unique pointings
+    
+    args = parser.parse_args()
+    # get list of current directory
+    flist1 = os.listdir()
+    flist1.sort()
 
-subdirs = ['plots','shortexposure']
-hafilters = ['Halpha','Ha6657']
-for p in upointings:
-    print(p)
-    if not(os.path.exists(p)):
-        os.mkdir(p)
-    if os.path.exists(p+'-r'):
-        os.system('mv '+p+'-r/* '+p+'/.')
-        os.system('rmdir '+p+'-r')
-    for h in hafilters:
-        hadir = p+'-'+h
-        if os.path.exists(hadir):
-            os.system('mv {}/* {}/.'.format(hadir,p))
-            for s in subdirs:
-                subdirpath = '{}-{}/{}'.format(p,h,s)
-                if os.path.exists(subdirpath):
-                    command = 'mv {}/* {}/{}/.'.format(subdirpath,p,s)
-                    print(command)
-                    os.system(command)
-                os.system('rmdir '+subdirpath)
-            os.system('rmdir {}'.format(hadir))
+    # get list of pointings - cut off filter from dir name
+    pointings = []
+    for f in flist1:
+        if( (f.find('pointing') > -1) | (f.find('target') > -1)) & (f.find('-') > -1):
+            pointings.append(f.split('-')[0])
+
+    # pointings will now have two listing for each filter b/c of r and Halpha
+    upointings = set(pointings) # unique pointings
+    print(f"found {len(upointings)} unique pointings")
+    
+    subdirs = ['plots','shortexposure']
+    hafilters = ['Halpha','Ha6657']
+    for p in upointings:
+        print(f"working on {p}")
+        if not(os.path.exists(p)):
+            if args.testing:
+                print(f"os.mkdir({p})")
+            else:
+                os.mkdir(p)
+        if os.path.exists(p+'-r'):
+            if args.testing:
+                print('mv '+p+'-r/* '+p+'/.')
+                print('rmdir '+p+'-r')
+            else:
+                os.system('mv '+p+'-r/* '+p+'/.')
+                os.system('rmdir '+p+'-r')
+        for h in hafilters:
+            hadir = p+'-'+h
+            if os.path.exists(hadir):
+                if args.testing:
+                    print('mv {}/* {}/.'.format(hadir,p))
+                else:
+                    os.system('mv {}/* {}/.'.format(hadir,p))
+                for s in subdirs:
+                    subdirpath = '{}-{}/{}'.format(p,h,s)
+                    if os.path.exists(subdirpath):
+                        command = 'mv {}/* {}/{}/.'.format(subdirpath,p,s)
+                        if args.testing:
+                            print(command)
+                        else:
+                            os.system(command)
+                    if args.testing:
+                        print('rmdir '+subdirpath)
+                    else:
+                        os.system('rmdir '+subdirpath)
+                if args.testing:
+                    print('rmdir {}'.format(hadir))
+                else:
+                    os.system('rmdir {}'.format(hadir))
 
 
 
