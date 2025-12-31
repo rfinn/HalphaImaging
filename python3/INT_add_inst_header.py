514a4a4fa28bbd9657b9387da801b5ab4e8e090b
rfinn <rfinn@siena.edu>
2025-12-31 14:15:39 -0500

    - working on #69 - writing wrapper to run the program in serial - didn't seem like parallel :(

--- python3/INT_add_inst_header.py
+++ python3/INT_add_inst_header.py
@@ -35,6 +35,24 @@ for i in range(len(matchstrings)):
             hdu.writeto(f,overwrite=True,output_verify='ignore')
             
         except TypeError: # getting buffer too small - maybe files are corrupted?
-            print(f"OH NO! astropy won't write {f}, file may be corrupted.  continuing on...")
+            print(f"OH NO! astropy won't write {f}, file may be corrupted.")
+            if '2019feb' in os.getcwd():
+                # look for earlier version
+                print(f"\tlooking for an earlier version of file {f}")
+                try:
+                    if 'WFC.r.' in f:
+                        altfile = f"/data-pool/Halpha/processed/2019febINT-all/allimages/rband/{f}"
+                    else:
+                        altfile = f"/data-pool/Halpha/processed/2019febINT-all/allimages/Halpha/{f}"
+                    if os.path.exists(altfile):
+                        hdu.close() # close first attempt
+                        os.system(f"cp {altfile} .")
+                        hdu = fits.open(f)
+                        hdu[0].header.set('INSTRMNT',instruments[i])
+                        try:
+                            hdu.writeto(f,overwrite=True,output_verify='ignore')
+                        except TypeError:
+                            print("\tso sorry, but was not able to get alt version of file to work")
+                    # now try 
         hdu.close()
         j+= 1
