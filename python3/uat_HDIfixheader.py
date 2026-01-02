3c621c25e72ebe6107d01a428bc446496d581b33
rfinn <rfinn@siena.edu>
2026-01-02 15:42:19 -0500

    - working on #71 - updated list of header fields

--- python3/uat_HDIfixheader.py
+++ python3/uat_HDIfixheader.py
@@ -139,7 +139,7 @@ for f in files:
                        'AIRMASS',
                        'DATE',
                        'OBJECT',
-                       'INSTRUMENT',
+                       'INSTRUME',
                        'FILENAME',
                        'OBSERVER']
 
