570a18f52ac60f7c8cf3007c2961349f911b5eb2
rfinn <rfinn@siena.edu>
2026-01-10 17:47:30 -0500

    working on #69 - creating constant weight images, which are the combination of sky noise and the PANSCALE scale factor.

---
+++ python3/INT_make_constants_weights_wrapper.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""
+OVERVIEW:
+- this is a wrapper for INT_make_constant_weights.py
+- this is intended to be used with gnu parallel
+
+USAGE:
+
+
+
+PROCEDURE:
+directory is passed on command line
+
+move to directory
+
+run INT_make_constant_weights.py
+
+move back to parent directory
+
+"""
+import os
+import sys
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description ="This program measures the offset of each ccd relative to PanSTARRS, calculates the scaling required to bring the ccds onto a uniform scale, and adds a keyword PANSCALE to the image headers.  PANSCALE can be used as the flux scaling keyword with SWarp.")
+    parser.add_argument('--dirlist', dest = 'dirlist', default = 'dirlist', help = 'file containing list of directories to run on.  default is dirlist')
+    parser.add_argument('--filestring', dest = 'filestring', default = 'mWFC', help = 'Filestring to pass into INT_get_ccd_offsets.py.  Usually this is mWFC or WFC.')        
+    parser.add_argument('--testing', dest = 'testing', default = False, action='store_true', help = 'Will run on one directory only.')
+    
+    args = parser.parse_args()
+    
+    dirlist = open(args.dirlist, 'r')
+
+    for d in dirlist:
+        subdir = d.rstrip('\n')
+
+        # move to directory
+        topdir = os.getcwd()
+
+        os.chdir(subdir)
+
+        # run INT_add_inst_header.py
+        os.system(f"python ~/github/HalphaImaging/python3/INT_make_constant_weights.py --filestring {args.filestring} --verbose")
+        # move back to parent directory
+        os.chdir(topdir)
+
+        if args.testing:
+            sys.exit()
