852efa3aa96ef19c40d3e6ae04dbae195795259b
rfinn <rfinn@siena.edu>
2026-02-12 17:50:38 -0500

    working on #69 - checking to see if spatial residuals with getzp.py are actually due to using a background mesh size that is too big.  The flux residuals from getzp fitting (left panel) track the variations in the sky.  I was using a pretty big BACK_SIZE, and this could miss the variations in the sky.  This will skew stellar fluxes, increasing the SE flux where the sky is high and decreasing it where the sky is low.

--- python3/getzp.py
+++ python3/getzp.py
@@ -489,7 +489,7 @@ class getzp():
         fwhm = np.median(self.secat['FWHM_IMAGE'])*self.pixelscale
             
             
-        t = f'sex {self.image} -c {defaultcat} -CATALOG_NAME {catdir}/{froot}.cat -BACK_SIZE 128 -BACKPHOTO_TYPE LOCAL -MAG_ZEROPOINT 0 -SATUR_LEVEL {str(ADUlimit)} -SEEING_FWHM {str(fwhm)}'
+        t = f'sex {self.image} -c {defaultcat} -CATALOG_NAME {catdir}/{froot}.cat -BACK_SIZE 128 -BACKPHOTO_TYPE LOCAL -MAG_ZEROPOINT 0 -SATUR_LEVEL {str(ADUlimit)} -SEEING_FWHM {str(fwhm):.2f}'
         if float(fwhm) == 0:
             print('WARNING: measured FWHM is zero!')
             if self.verbose:
